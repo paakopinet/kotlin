573b4b8ecf6bc69ab21a16f539323e21de391ea7
Mads Ager <ager@google.com>
2020-06-24 17:06:50 +0200

    [JVM] Extend stepping tests with <clinit> stepping.
    In addition, made the stepping information for constructor calls
    consistent across JVM_IR and JVM. For JVM_IR that stepping behavior
    is consistent for enum constructor calls in <clinit> for JVM it
    is not.

--- compiler/testData/debug/stepping/initBlocksCompanion.kt
+++ compiler/testData/debug/stepping/initBlocksCompanion.kt
@@ -38,6 +38,17 @@ fun box() {
 
 // LINENUMBERS
 // test.kt:29 box
+// test.kt:7 <clinit>
+// test.kt:8 <clinit>
+// test.kt:9 <clinit>
+// test.kt:11 <clinit>
+// test.kt:13 <clinit>
+// test.kt:15 <clinit>
+// test.kt:16 <clinit>
+// test.kt:17 <clinit>
+// test.kt:19 <clinit>
+// test.kt:21 <clinit>
+// test.kt:22 <clinit>
 // test.kt:11 getX
 // LINENUMBERS JVM
 // test.kt:11 getX
